c403133950d83ec27fc05a27a23b22afffd41714
Daniel Azulay <you@example.comdanielazulay20@gmail.com>
2021-06-17 10:37:09 -0300

    git page fix

--- js/game.js
+++ js/game.js
@@ -65,10 +65,10 @@ class PuzzleGame{
        
         if(escolha==='opcao1'){
        
-          jogos="url(./img/jogo2.png)"
+          jogos="url(../img/jogo2.png)"
         }else{
 
-          jogos="url(./img/jogo1.jpg)"
+          jogos="url(../img/jogo1.jpg)"
         }
 
          if(cont ===this.pieces.length) {
